c6fe35800a6351e300918462c5616726dfac0bae
Pankaj2755 <20025882@students.koi.edu.au>
2025-12-08 10:33:57 +1100

    Update Program.py

--- Program.py
+++ Program.py
@@ -2,14 +2,14 @@
 
 def display_person_info():
     # First person's details
-    name1 = "Alice"
+    name1 = "Pankaj"
     age1 = 30
-    print(f"Name 1: {Nitesh}, Age: {23}")
+    print(f"Name 1: {name1}, Age: {age1}")
 
     # Second person's details
-    name2 = "Bob"
+    name2 = "Nitesh"
     age2 = 25
-    print(f"Name 2: {Pankaj}, Age: {24}")
+    print(f"Name 2: {name2}, Age: {age2}")
 
 # Run the function
 display_person_info()
